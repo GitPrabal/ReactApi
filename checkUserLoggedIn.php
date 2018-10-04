0aa6e1adb4a3bb171baa8eb7802a9b7bc4a03ad2
cygnet <prabal1.gupta@gmail.com>
2018-10-04 17:11:25 +0530

    Initial Commit

---
+++ checkUserLoggedIn.php
@@ -0,0 +1,36 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+
+include 'config.php';
+
+$email    = $_GET['email'];
+$password = $_GET['password'];
+
+
+
+
+
+$sql    = "SELECT *,count(*) as count FROM registration where email='$email' and password='$password' "; 
+$result = mysqli_query($con,$sql);
+
+$result = mysqli_fetch_assoc($result);
+
+$count  = $result['count'];
+$email  = $result['email'];
+$fullname  = $result['fullname'];
+$user_id   = $result['user_id'];
+
+if($count > 0){
+	$result =array("user_id"=>$user_id,"email"=>$email, "msg"=>"Logged In Successfully","status"=>"200","flag"=>true);
+	$json = json_encode($result);
+	echo $json ;
+}else{
+	$result =array("msg"=>"Invalid Credentials","status"=>"404","flag"=>false);
+	$json = json_encode($result);
+	echo $json ;
+}
+
+
+mysqli_close($con);
+	
+?>
---
+++ insert_data.php
@@ -0,0 +1,66 @@
+<?php
+
+header('Access-Control-Allow-Origin: *'); 
+
+$time    = microtime(); 
+$time2   = time();  
+$result  =  ceil(str_replace('.','',$time));
+$user_id = $result + $time2;
+
+	include 'config.php';
+
+	$json = json_decode(file_get_contents("php://input"));
+
+    $full_name = $json->fullname;
+    $email = $json->email;
+	$password = $json->pass;
+	
+	if( !empty($full_name) && !empty($email) && !empty($password) &&  $full_name!=='' && $email!=='' && $password!==''){
+
+		/* Check Wheather  */
+
+	$count  = checkUserAvailable($con,$email);	
+
+	if($count > 0){
+		$result =array("msg"=>"Email id already in use","status"=>"300");
+		$json = json_encode($result);
+		echo $json ;
+		return;
+	}
+
+	$Sql_Query =  "INSERT INTO `registration`(`user_id`,`fullname`, `email`, `password`) VALUES ('$user_id','$full_name','$email','$password')";
+
+
+	$Sql_Query1 = "INSERT INTO `user_details`(`user_id`,`full_name`,`email`,`address`,`phone_no`,`profile_pic`) VALUES 
+	('".$user_id."','".$full_name."','".$email."','','','')";
+
+
+ 
+	 if(  mysqli_query($con,$Sql_Query) &&  mysqli_query($con,$Sql_Query1) ){
+		$result =array("msg"=>"Account Created Succesfully","status"=>"200");
+		$json = json_encode($result);
+		echo $json ;
+	 }
+	 else{
+		 $result =array("msg"=>"Someting wents wrong","status"=>"400");
+		 $json = json_encode($result);
+		 echo $json ;
+	 }
+	mysqli_close($con);
+}else{
+	     $result =array("msg"=>"Parameter mismatched","status"=>"400");
+		 $json = json_encode($result);
+		 echo $json ;
+}
+
+function checkUserAvailable($con,$email){
+
+	$userExist_Query =  "Select count(*) as count from `registration` where email='$email'";
+	$result = mysqli_query($con,$userExist_Query);
+	$result = mysqli_fetch_assoc($result);
+	$count  = $result['count'];
+	return $count;
+
+}
+
+?>
