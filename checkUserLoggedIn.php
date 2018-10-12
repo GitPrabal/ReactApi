fb33449a1fdc116e11550b53cc5e02d05fc551ae
cygnet <prabal1.gupta@gmail.com>
2018-10-12 18:07:29 +0530

    first commit

--- checkUserLoggedIn.php
+++ checkUserLoggedIn.php
@@ -10,21 +10,22 @@ $email    = $_GET['email'];
 $password = $_GET['password'];
 
 
-
-
-
 $sql    = "SELECT *,count(*) as count FROM registration where email='$email' and password='$password' "; 
 $result = mysqli_query($con,$sql);
 
 $result = mysqli_fetch_assoc($result);
 
-$count  = $result['count'];
-$email  = $result['email'];
+$count     = $result['count'];
+$email     = $result['email'];
 $fullname  = $result['fullname'];
 $user_id   = $result['user_id'];
+$reg_date  = $result['reg_date'];
+$reg_date  = explode(' ', $reg_date);
+$reg_date  = $reg_date[0];
+$reg_date = date("M jS, Y", strtotime($reg_date));
 
 if($count > 0){
-	$result =array("user_id"=>$user_id,"email"=>$email, "msg"=>"Logged In Successfully","status"=>"200","flag"=>true,'fullname'=>$fullname);
+	$result =array("user_id"=>$user_id,"email"=>$email, "msg"=>"Logged In Successfully","status"=>"200","flag"=>true,'fullname'=>$fullname,'reg_date'=>$reg_date);
 	$json = json_encode($result);
 	echo $json ;
 }else{
